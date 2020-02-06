c9a3c00216e3618f39ba1fc23f3f502f3c172fa5
Lê Tấn Phát <phatle@rta.vn>
2020-02-06 11:35:34 +0700

    Init project.

---
+++ ManageStore/ManageStore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ManageStore
+//
+//  Created by Phat Tan Le on 2/6/20.
+//  Copyright © 2020 ManageStore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
